c701e1bad66a3d727eb99698888ddd42d9ce4bd2
f varas <tatico@gmail.com>
2015-01-04 14:36:06 +0100

    Update fireglow.py

--- fireglow.py
+++ fireglow.py
@@ -8,12 +8,14 @@ pyglow.all(0)
 
 
 colors = ['white','blue','green','yellow','orange','red']
-for b in range(1,500):
-	print b
-	rbright = random.randrange(10,255,10)
-	rcolor = random.choice(colors)
-	pyglow.color(rcolor, brightness = rbright)
-	time.sleep(0.1)
-	pyglow.all(0)  ## if you comment this line, leds stay on util a new color/brightness value is given)
-
+try:
+	for b in range(1,500):
+		print b
+		rbright = random.randrange(10,255,10)
+		rcolor = random.choice(colors)
+		pyglow.color(rcolor, brightness = rbright)
+		time.sleep(0.1)
+		pyglow.all(0)  ## if you comment this line, leds stay on util a new color/brightness value ig given)
+except KeyboardInterrupt:
+	pyglow.all(0)
 pyglow.all(0)
